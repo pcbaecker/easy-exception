5d978c431df942f03c74c345221745fdfe15dc4c
Philipp C. Baecker <pcbaecker@gmail.com>
2019-02-18 08:56:45 +0100

    Android (#5)
    * Android project created
    
    * On android working

--- src/Log.cpp
+++ src/Log.cpp
@@ -2,18 +2,26 @@
 #include <fstream>
 #include <csignal>
 
+#ifdef __ANDROID__
+#include <android/log.h>
+#include <sstream>
+#endif
+
 namespace ee {
 
+#ifdef __ANDROID__
+    std::stringstream ss;
+#endif
     static std::string logFolder;
     static std::string logFilename;
     std::recursive_mutex Log::Mutex;
     std::atomic_uint16_t Log::SuspendLoggingCounter = 0;
     std::map<std::thread::id, std::list<LogEntry>> Log::LogThreadMap;
-    std::map<LogLevel,std::function<void(const LogEntry&)>> Log::CallbackMap;
-    std::map<LogLevel, std::ostream*> Log::OutStreamMap;
-    std::map<uint8_t,std::shared_ptr<LogRetentionPolicy>> Log::LogRetentionPolicies;
+    std::map<LogLevel, std::function<void(const LogEntry &)>> Log::CallbackMap;
+    std::map<LogLevel, std::ostream *> Log::OutStreamMap;
+    std::map<uint8_t, std::shared_ptr<LogRetentionPolicy>> Log::LogRetentionPolicies;
 
-    void logLevelHandler(const LogEntry& logEntry) noexcept {
+    void logLevelHandler(const LogEntry &logEntry) noexcept {
         // We suspend logging for the whole scope of this function
         SuspendLogging suspendLogging;
 
@@ -23,7 +31,8 @@ namespace ee {
         // Create filename
         if (logFilename.empty()) {
             auto timestamp = std::chrono::system_clock::now();
-            auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(timestamp.time_since_epoch()).count();
+            auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(
+                    timestamp.time_since_epoch()).count();
             logFilename = logFolder + "ee-log-" + std::to_string(microseconds) + ".log";
         }
 
@@ -53,9 +62,10 @@ namespace ee {
             if (e != nullptr) {
                 std::rethrow_exception(e);
             } else {
-                ee::Log::log(ee::LogLevel::Fatal, "", __PRETTY_FUNCTION__, "Terminate called", {}, ee::Stacktrace::create());
+                ee::Log::log(ee::LogLevel::Fatal, "", __PRETTY_FUNCTION__, "Terminate called", {},
+                             ee::Stacktrace::create());
             }
-        } catch (ee::Exception& e) {
+        } catch (ee::Exception &e) {
             // In case of an ee::Exception
             ee::Log::log(ee::LogLevel::Fatal, e);
         } catch (std::exception &e) {
@@ -63,7 +73,8 @@ namespace ee {
             ee::Log::log(ee::LogLevel::Fatal, "", "Uncaught exception", e.what(), {}, ee::Stacktrace::create());
         } catch (...) {
             // In case some unknown exception
-            ee::Log::log(ee::LogLevel::Fatal, "", "Uncaught exception", "Unknown exeception", {}, ee::Stacktrace::create());
+            ee::Log::log(ee::LogLevel::Fatal, "", "Uncaught exception", "Unknown exeception", {},
+                         ee::Stacktrace::create());
         }
 
         // A file for this kind of error will automatically be created through the LogLevelHandler
@@ -77,10 +88,10 @@ namespace ee {
             const std::string &classname,
             const std::string &method,
             const std::string &message,
-            const std::vector<Note>& notes,
-            const std::optional<std::shared_ptr<Stacktrace>>& stacktrace) noexcept {
+            const std::vector<Note> &notes,
+            const std::optional<std::shared_ptr<Stacktrace>> &stacktrace) noexcept {
         // Every thread stores its own pointer to the log list
-        thread_local std::list<LogEntry>* pList = nullptr;
+        thread_local std::list<LogEntry> *pList = nullptr;
 
         // Check if a pointer to the list is already generated
         if (pList == nullptr) { // NOLINT
@@ -97,14 +108,44 @@ namespace ee {
         }
 
         // Create a LogEntry in the thread specific list
-        auto& logEntry = pList->emplace_back(logLevel, classname, method, message, notes, stacktrace, std::chrono::system_clock::now());
+        auto &logEntry = pList->emplace_back(logLevel, classname, method, message, notes, stacktrace,
+                                             std::chrono::system_clock::now());
 
         // Check if we should display a copy of the logEntry in an outstream (e.g.: std::cout)
         if (OutStreamMap.count(logLevel)) {
-            auto& stream = *OutStreamMap.at(logLevel);
+            auto &stream = *OutStreamMap.at(logLevel);
             logEntry.write(stream);
             stream << std::endl << std::endl;
         }
+
+#ifdef __ANDROID__
+        // On android we print the log to logcat
+        logEntry.write(ss);
+        std::string s = ss.str();
+        ss.str("");
+        switch (logLevel) {
+            default:
+            case ee::LogLevel::Trace:
+                __android_log_print(ANDROID_LOG_VERBOSE, "EasyException", "%s", s.c_str());
+                break;
+
+            case ee::LogLevel::Info:
+                __android_log_print(ANDROID_LOG_INFO, "EasyException", "%s", s.c_str());
+                break;
+
+            case ee::LogLevel::Warning:
+                __android_log_print(ANDROID_LOG_WARN, "EasyException", "%s", s.c_str());
+                break;
+
+            case ee::LogLevel::Error:
+                __android_log_print(ANDROID_LOG_ERROR, "EasyException", "%s", s.c_str());
+                break;
+
+            case ee::LogLevel::Fatal:
+                __android_log_print(ANDROID_LOG_FATAL, "EasyException", "%s", s.c_str());
+                break;
+        }
+#endif
 
         // Check if we have a callback function for this LogLevel
         if (CallbackMap.count(logLevel)) {
@@ -121,7 +162,7 @@ namespace ee {
                 exception.getMessage(),
                 exception.getNotes(),
                 exception.getStacktrace()
-                );
+        );
     }
 
     const std::map<std::thread::id, std::list<LogEntry>> &Log::getLogThreadMap() noexcept {
@@ -135,7 +176,7 @@ namespace ee {
         std::lock_guard<std::recursive_mutex> mutex(Log::Mutex);
 
         // Iterate through the different threads
-        for (auto& thread : LogThreadMap) {
+        for (auto &thread : LogThreadMap) {
             // Just add the number of log entries of every thread to the total number
             numberOfLogEntries += thread.second.size();
         }
@@ -151,7 +192,7 @@ namespace ee {
         std::lock_guard<std::recursive_mutex> mutex(Log::Mutex);
 
         // Iterate through the different threads
-        for (auto& thread : Log::LogThreadMap) {
+        for (auto &thread : Log::LogThreadMap) {
             // We cant remove the lists from the parent map because pointers to that lists are stored in every thread
             // and we would make them invalid. Instead we just clear each list and the pointers remain valid.
             thread.second.clear();
@@ -182,17 +223,17 @@ namespace ee {
         }
 
         // Iterate through all threads
-        for (auto& thread : LogThreadMap) {
+        for (auto &thread : LogThreadMap) {
             // Check if this thread has at least one log entry
             if (!thread.second.empty()) {
                 // Write the headline for this thread
-                for (int i = 0; i < 32; i++) {file << '#';}
+                for (int i = 0; i < 32; i++) { file << '#'; }
                 file << "### " << thread.first << " ";
-                for (int i = 0; i < 32; i++) {file << '#';}
+                for (int i = 0; i < 32; i++) { file << '#'; }
                 file << "\n";
 
                 // Iterate through all log entries for this thread
-                for (auto& logEntry : thread.second) {
+                for (auto &logEntry : thread.second) {
                     // Write log entry to file
                     logEntry.write(file);
 
@@ -218,12 +259,14 @@ namespace ee {
         return OutStreamMap;
     }
 
-    void Log::applyDefaultConfiguration(const std::string& pathToLogFolder) noexcept {
+    void Log::applyDefaultConfiguration(const std::string &pathToLogFolder) noexcept {
         // Register the outstream
+#ifndef __ANDROID__
         registerOutstream(LogLevel::Info, std::cout);
         registerOutstream(LogLevel::Warning, std::cerr);
         registerOutstream(LogLevel::Error, std::cerr);
         registerOutstream(LogLevel::Fatal, std::cerr);
+#endif
 
         // Register signal handler
         std::signal(SIGSEGV, signalHandler);
@@ -238,7 +281,7 @@ namespace ee {
         std::set_terminate(terminateHandler);
 
         // Store the folder where we want to store logs
-        if (!pathToLogFolder.empty() && pathToLogFolder[pathToLogFolder.size()-1] != '/') {
+        if (!pathToLogFolder.empty() && pathToLogFolder[pathToLogFolder.size() - 1] != '/') {
             logFolder = pathToLogFolder + "/";
         } else {
             logFolder = pathToLogFolder;
@@ -273,9 +316,9 @@ namespace ee {
         std::lock_guard<std::recursive_mutex> mutex(Log::Mutex);
 
         // Go through the different threads
-        for (auto& thread : LogThreadMap) {
+        for (auto &thread : LogThreadMap) {
             // Init the log retention policies
-            for (auto& policy : LogRetentionPolicies) {
+            for (auto &policy : LogRetentionPolicies) {
                 policy.second->init();
             }
 
@@ -284,7 +327,7 @@ namespace ee {
             while (it != thread.second.rend()) {
                 // Go through all policies
                 bool removeLogEntry = false;
-                for (auto& policy : LogRetentionPolicies) {
+                for (auto &policy : LogRetentionPolicies) {
                     if (!policy.second->retain(*it)) {
                         // This log entry should be deleted by this policy
                         removeLogEntry = true;
@@ -313,8 +356,8 @@ namespace ee {
         std::map<LogLevel, size_t> map;
 
         // Go through all threads and log entries
-        for (auto& thread : LogThreadMap) {
-            for (auto& logEntry : thread.second) {
+        for (auto &thread : LogThreadMap) {
+            for (auto &logEntry : thread.second) {
                 // Check if we already have an entry for this log level
                 if (map.count(logEntry.getLogLevel())) {
                     // Increase entry
